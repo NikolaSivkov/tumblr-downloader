5c81fc5bb036c1a44582f14ed2a7e2add0cefbfd
Aviatrix <erasrz@gmail.com>
2012-01-14 21:59:21 +0200

    Initial commit

---
+++ tumblr-downloader/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace tumblr_downloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.usernamBox = new System.Windows.Forms.TextBox();
+            this.SelectFolderDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.pathBox = new System.Windows.Forms.TextBox();
+            this.selectFolder = new System.Windows.Forms.Button();
+            this.downloadBtn = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.totalImagesLbl = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.progressBar = new System.Windows.Forms.ToolStripProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(192, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = ".tumblr.com";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(13, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "http://";
+            // 
+            // usernamBox
+            // 
+            this.usernamBox.Location = new System.Drawing.Point(57, 9);
+            this.usernamBox.Name = "usernamBox";
+            this.usernamBox.Size = new System.Drawing.Size(129, 20);
+            this.usernamBox.TabIndex = 2;
+            // 
+            // SelectFolderDialog
+            // 
+            this.SelectFolderDialog.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            // 
+            // pathBox
+            // 
+            this.pathBox.Location = new System.Drawing.Point(13, 45);
+            this.pathBox.Name = "pathBox";
+            this.pathBox.Size = new System.Drawing.Size(173, 20);
+            this.pathBox.TabIndex = 3;
+            // 
+            // selectFolder
+            // 
+            this.selectFolder.Location = new System.Drawing.Point(192, 42);
+            this.selectFolder.Name = "selectFolder";
+            this.selectFolder.Size = new System.Drawing.Size(90, 23);
+            this.selectFolder.TabIndex = 4;
+            this.selectFolder.Text = "Slelect Folder";
+            this.selectFolder.UseVisualStyleBackColor = true;
+            this.selectFolder.Click += new System.EventHandler(this.selectFolder_Click);
+            // 
+            // downloadBtn
+            // 
+            this.downloadBtn.Location = new System.Drawing.Point(292, 6);
+            this.downloadBtn.Name = "downloadBtn";
+            this.downloadBtn.Size = new System.Drawing.Size(89, 59);
+            this.downloadBtn.TabIndex = 5;
+            this.downloadBtn.Text = "Go Get \'em!";
+            this.downloadBtn.UseVisualStyleBackColor = true;
+            this.downloadBtn.Click += new System.EventHandler(this.downloadBtn_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.totalImagesLbl,
+            this.toolStripStatusLabel2,
+            this.progressBar});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 92);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(391, 22);
+            this.statusStrip1.TabIndex = 11;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(84, 17);
+            this.toolStripStatusLabel1.Text = "Total Images : ";
+            // 
+            // totalImagesLbl
+            // 
+            this.totalImagesLbl.Name = "totalImagesLbl";
+            this.totalImagesLbl.Size = new System.Drawing.Size(13, 17);
+            this.totalImagesLbl.Text = "0";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(74, 17);
+            this.toolStripStatusLabel2.Text = "Downloaded";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(100, 16);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 114);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.downloadBtn);
+            this.Controls.Add(this.selectFolder);
+            this.Controls.Add(this.pathBox);
+            this.Controls.Add(this.usernamBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox usernamBox;
+        private System.Windows.Forms.FolderBrowserDialog SelectFolderDialog;
+        private System.Windows.Forms.TextBox pathBox;
+        private System.Windows.Forms.Button selectFolder;
+        private System.Windows.Forms.Button downloadBtn;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar progressBar;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel totalImagesLbl;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ tumblr-downloader/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Threading;
+using System.Windows.Forms;
+using tumblr_downloader;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using Hammock;
+using Hammock.Serialization;
+using Hammock.Model;
+using Hammock.Streaming;
+using Timer = System.Threading.Timer;
+
+
+namespace tumblr_downloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void selectFolder_Click(object sender, EventArgs e)
+        {
+            SelectFolderDialog.ShowDialog();
+
+            pathBox.Text = SelectFolderDialog.SelectedPath.ToString();
+            AppSettings.Default.downlaodpath = SelectFolderDialog.SelectedPath.ToString();
+            AppSettings.Default.Save();
+        }
+
+        private void downloadBtn_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
+            timer1.Start();
+
+        }
+
+        public string User { get; set; }
+        private Uri URL;
+        private SynchronizedCollection<Uri> allImages = new SynchronizedCollection<Uri>();
+
+
+        public void fetchXml()
+        {
+            User = usernamBox.Text.Replace(" ", "").Replace("/", "").Replace(".", "");
+            URL = new Uri("http://" + User + ".tumblr.com/api/read/json");
+
+            int max = 0;
+            var client = new RestClient();
+            client.Authority = URL.ToString();
+            var request = new RestRequest();
+            request.AddParameter("type", "photo");
+            request.AddParameter("num", "50");
+            request.AddParameter("filter", "text");
+            var r1 = client.Request(request);
+            var t = r1.Content.ToString().Replace("var tumblr_api_read = ", "");
+            var firstResponse = JsonParser.FromJson(t);
+            max = Convert.ToInt32(firstResponse["posts-total"]);
+            // to eventually make each fetch a separate request
+            for (int i = 0; i < max; i += 51)
+            {
+                if (i != 0)
+                {
+                    request.AddParameter("start", i.ToString());
+                }
+                var r2 = client.Request(request);
+                var t2 = r2.Content.ToString().Replace("var tumblr_api_read = ", "");
+                var Response = JsonParser.FromJson(t2);
+
+                getUrls(Response.ToDictionary(x => x.Key, x => x.Value));
+            }
+          
+
+        }
+
+        private void getUrls(Dictionary<string, object> dictionary)
+        {
+            var posts = dictionary.Last().Value;
+
+            foreach (var o in posts as List<object>)
+            {
+                var temp = o as Dictionary<string, object>;
+
+                if (String.IsNullOrEmpty(temp["photo-url-1280"].ToString()) ||
+                    String.IsNullOrWhiteSpace(temp["photo-url-1280"].ToString()))
+                {
+                    allImages.Add(new Uri(temp["photo-url-500"].ToString()));
+                }
+                else
+                {
+                    allImages.Add(new Uri(temp["photo-url-1280"].ToString()));
+                }
+            }
+           
+        }
+
+        private void DownloadFile()
+        {
+            if (allImages.Any())
+            {
+                for (int i = 0; i < allImages.Count; i++)
+                {
+                    WebClient client = new WebClient();
+                    client.DownloadFileCompleted += Completed;
+                    string FileName = allImages[i].Segments.Last().ToString();
+                    if (FileName.EndsWith(".jpg") || FileName.EndsWith(".png") || FileName.EndsWith(".gif") )
+                    {
+                        
+                        client.DownloadFileAsync(allImages[i],
+                                              AppSettings.Default.downlaodpath + @"\" + FileName );
+                    }
+                    else
+                    {
+                        client.DownloadFileAsync(allImages[i], AppSettings.Default.downlaodpath + @"\" + FileName+".jpg");
+                    }
+                }
+            }
+        }
+
+        private void Completed(object sender, AsyncCompletedEventArgs e)
+        {
+            progressBar.Value++;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(AppSettings.Default.downlaodpath))
+            {
+                pathBox.Text = AppSettings.Default.downlaodpath;
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            fetchXml();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            totalImagesLbl.Text = allImages.Count.ToString();
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            timer1.Stop();
+            progressBar.Maximum = allImages.Count;
+            totalImagesLbl.Text = allImages.Count.ToString();
+            DownloadFile();
+        }
+    }
+}
